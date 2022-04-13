519f33a556bcae25cb9fa01aba3064a6af3a57ba
Saha555339 <sasha555339@mail.ru>
2022-04-13 20:45:01 +0300

    upd1

--- app/views.py
+++ app/views.py
@@ -8,20 +8,38 @@ QUESTIONS = [
     } for i in range(5)
 ]
 
-def index(request):
-    return render(request, "index.html", {"questions": QUESTIONS})
+TAGS = [
+    {
+        "name": f"Tag {i}"
+    } for i in range(3)
+]
 
-def question(request, i:int):
-    return render(request, "question_page.html", {"question": QUESTIONS[i]})
+TOP_MEMBERS = [
+    {
+        "name": f"Member {1}"
+    } for i in range(2)
+]
+
+
+def index(request):
+    return render(request, "index.html", {"questions": QUESTIONS, "tags": TAGS, "top_members": TOP_MEMBERS})
+
+
+def question(request, i: int):
+    return render(request, "question_page.html", {"question": QUESTIONS[i], "tags": TAGS, "top_members": TOP_MEMBERS})
+
 
 def ask(request):
-    return render(request, "ask_question.html")
+    return render(request, "ask_question.html", {"tags": TAGS, "top_members": TOP_MEMBERS})
+
 
 def log_in(request):
-    return render(request, "log_in.html")
+    return render(request, "log_in.html", {"tags": TAGS, "top_members": TOP_MEMBERS})
+
 
 def registration(request):
-    return render(request, "registration.html")
+    return render(request, "registration.html", {"tags": TAGS, "top_members": TOP_MEMBERS})
+
 
 def settings(request):
-    return render(request, "settings.html")
+    return render(request, "settings.html", {"tags": TAGS, "top_members": TOP_MEMBERS})
